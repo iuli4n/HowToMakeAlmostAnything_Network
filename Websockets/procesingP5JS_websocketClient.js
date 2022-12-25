2b38ff2f8c74c489dca75f0c039fd5d888ec8ccb
Iulian Radu <iulian_radu@gse.harvard.edu>
2022-12-25 00:12:34 -0500

    Update procesingP5JS_websocketClient.js

--- Websockets/procesingP5JS_websocketClient.js
+++ Websockets/procesingP5JS_websocketClient.js
@@ -1,35 +1,59 @@
 // Connects to the websocket server and moves an ellipse on the screen based on the number it receives from the websocket
 
-// where the serial server is (your local machine):
-var host = 'localhost:8000';
-var socket; // the websocket
-var sensorValue = 0; // the sensor value
+
+//------------- WEBSOCKET START --------------
+// HOW TO INTEGRATE INTO YOUR OWN SKETCH: 
+//   1. Copy this whole part into your p5js sketch
+//   2. Add ws_setup() to the setup() function as shown below
+//   3. Then access the wsData1,2,3 variables
+
+var ws_host = 'localhost:8000'; // address of the websocket server
+var ws_socket; // socket we'll be using
+var wsData1, wsData2, wsData3; // data we received from the socket
+
+function ws_setup() {
+  // connect to server:
+  ws_socket = new WebSocket('ws://' + ws_host);
+  // socket connection listener:
+  ws_socket.onopen = ws_sendIntro;
+  // socket message listener:
+  ws_socket.onmessage = ws_readMessage;
+  
+  print("Websocket server started");
+}
+
+function ws_readMessage(event) {
+  var msg = event.data; // read data from the onmessage event
+  //print("Received: "+msg); // print it
+
+  // Assume we're getting 3 numerical values separated by comma: X,Y,Z
+  
+  const arduinoDataSplit = msg.split(",");
+
+  wsData1 = parseInt(arduinoDataSplit[0]);
+  wsData2 = parseInt(arduinoDataSplit[1]);
+  wsData3 = parseInt(arduinoDataSplit[2]);
+}
+
+function ws_sendIntro() {
+  ws_socket.send("Hello");
+}
+
+//------------- WEBSOCKET END ----------
+
+
+
+
 
 function setup() {
   createCanvas(400, 400);
-  // connect to server:
-  socket = new WebSocket('ws://' + host);
-  // socket connection listener:
-  socket.onopen = sendIntro;
-  // socket message listener:
-  socket.onmessage = readMessage;
+  ws_setup();
 }
 
 function draw() {
   background("#2307AF");
   fill(255);
-  ellipse(sensorValue*10 % width, height / 2, 20, 20);
-  text(sensorValue, 20, 20);
+  ellipse(wsData1*10 % width, height / 2, 20, 20);
+  text(wsData1, 20, 20);
 }
 
-function sendIntro() {
-  // convert the message object to a string and send it:
-  socket.send("Hello");
-}
-
-function readMessage(event) {
-  // assumes it receives one number
-  var msg = event.data; // read data from the onmessage event
-  sensorValue = Number(msg);
-  println("Received: "+sensorValue); // print it
-}
